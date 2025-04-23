1e33d5f96c99afad620384752e24fb2a1832831f
unknown <germanfernandezblanco@gmail.com>
2025-04-23 22:08:00 +0200

    Server *

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "incomparable-kelpie-ba6430.netlify.app",
+    origin: "*",
+    // origin: "incomparable-kelpie-ba6430.netlify.app",
     // origin: "https://vfernandez24.github.io",
     // origin: "http://localhost:5173",
     credentials: true,
